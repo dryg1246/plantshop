388f87bdebabaa71dfc9607a5ea919e080b3ea4d
dryg1246 <smetdana12032007@gmail.com>
2023-11-19 14:07:32 +0100

    img

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Header} from "../components/mainpage/header/Header";
+import {Banner} from "../components/mainpage/main/banner/banner";
+
+export const Home = () => {
+    return (
+        <div>
+        </div>
+    )
+}
